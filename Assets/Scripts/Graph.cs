8a54669c5cc281778858ea37adb623bb9cf5f2f1
papercd <hpptknk10@hotmail.co.kr>
2025-07-27 20:49:28 +0900

    working on mathematical surfaces by the one and only Jasper Flick

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO.Compression;
 using UnityEngine;
 
 public class Graph : MonoBehaviour
@@ -12,18 +13,32 @@ public class Graph : MonoBehaviour
     [SerializeField, Range(10, 100)]
     int resolution = 10;
 
+    [SerializeField]
+    FunctionLibrary.FunctionName function;
+
+
     void Awake()
     {
         float step = 2f / resolution;
         Vector3 position = Vector3.zero;
         var scale = Vector3.one * step;
 
-        points = new Transform[resolution];
+        points = new Transform[resolution * resolution];
 
-        for (int i = 0; i < points.Length; i++)
+        for (int i = 0, x = 0 , z=0; i < points.Length; i++,x++)
         {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+            }
+
             Transform point = points[i] = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) * step - 1f;
+
+            position.x = (x + 0.5f) * step - 1f;
+
+            position.z = (z + 0.5f) * step - 1f;
+
             point.localScale = scale;
             point.localPosition = position;
             point.SetParent(transform, false);
@@ -34,11 +49,14 @@ public class Graph : MonoBehaviour
     void Update()
     {
         float time = Time.time;
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
+
         for (int i = 0; i < points.Length; i++)
         {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+
+            position.y = f(position.x,position.z,time);
             point.localPosition = position;
         }
     }
